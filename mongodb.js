38757d5cd1519c333e88e08afe5f877cf17a2def
julyyv <102019281+MeoDaoSi@users.noreply.github.com>
2023-02-09 10:11:55 +0700

    install mongoose

--- mongodb.js
+++ mongodb.js
@@ -1,4 +1,4 @@
-const {MongoClient} = require('mongodb');
+const {MongoClient, ObjectId} = require('mongodb');
 
 const url = 'mongodb://127.0.0.1:27017';
 const client = new MongoClient(url);
@@ -6,20 +6,20 @@ const databaseName = 'task_manager';
 
 async function main(){
     // await client.connect()
+
     const db = client.db(databaseName);
-    // const result = await db.collection('users').insertOne({
-    //     name: "Tinhle",
-    //     age: 20
-    // })
     // console.log(result.acknowledged);
-    const result = await db.collection('users').insertMany([
-        { name: "ton ngo no", age: 1256 },
-        { name: "ton hanh gia", age: 2566 },
-        { name: "gia hanh ton", age: 2415 }
-    ], { ordered: true })
+    // const result = await db.collection('users').insertMany([
+    //     { name: "ton ngo no", age: 1256 },
+    //     { name: "ton hanh gia", age: 2566 },
+    //     { name: "gia hanh ton", age: 2415 }
+    // ], { ordered: true })
+    // const result = await db.collection('users').find({name : 'ton ngo no'}).toArray();
+    const update = await db.collection('users').updateOne({ name : 'ton ngo no'},{ $set : { name : 'tinh'}})
+    console.log(update);
     return 'done.';
 }
 
 main()
-    .then(console.log)
+    .then( data => console.log(data) )
     .catch(console.error)
